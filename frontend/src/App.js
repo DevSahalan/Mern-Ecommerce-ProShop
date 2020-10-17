64c07332cbd8f519435b7aa39a004e2b70b940ba
sahalan hasan <mythsahalan@yahoo.com>
2020-10-18 02:51:59 +0600

    reviews and router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,23 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './components/HomeScreen'
+import ProductScreen from './components/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </>
+    <Router>
+      <switch>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} exact />
+          </Container>
+        </main>
+        <Footer />
+      </switch>
+    </Router>
   )
 }
 
